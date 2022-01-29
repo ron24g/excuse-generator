bbdbc30dfc39b7152f82b01b23726a221d3295a7
ron24g <ron24g@yahoo.com>
2022-01-29 15:29:54 +0000

    excuse generator 1st copy

--- src/app.js
+++ src/app.js
@@ -9,3 +9,34 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+// Returns a random integer from 0 to 9:
+/*console.log(Math.floor(Math.random() * when.length));
+console.log(action[0]);
+console.log(what[1]);
+console.log(when.length);
+console.log(when[Math.floor(Math.random() * when.length)]);
+console.log(action[Math.floor(Math.random() * action.length)]);*/
+
+let generateExcuse = () => {
+  var randomExcuse =
+    who[Math.floor(Math.random() * who.length)] +
+    " " +
+    action[Math.floor(Math.random() * action.length)] +
+    " " +
+    what[Math.floor(Math.random() * what.length)] +
+    " " +
+    when[Math.floor(Math.random() * when.length)];
+  return randomExcuse;
+};
+console.log(generateExcuse());
+document.getElementById("excuse").innerHTML = generateExcuse();
